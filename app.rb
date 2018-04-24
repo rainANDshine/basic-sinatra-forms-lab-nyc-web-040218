c56421da028ffab57c860ca81f66261f6c675b1a
Shun Yao <ysmiracle@hotmail.com>
2018-04-24 02:47:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  post '/newteam' do
+    @newteam << param[:name] << param[:coach] << param[:pg] << param[:sg] << param[:sf] << param[:pf] << param[:c]
+    erb :team
+  end
 
+  get '/team' do
+    erb :team
+  end
 
 end
